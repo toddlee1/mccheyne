94b0bc961ddbe82424ed08cfb341a15b1725f967
1004714 <todd.lee@sk.com>
2023-06-06 23:36:31 +0900

    Project Start

---
+++ src/main/java/com/todd/lee/mccheyne/MccheyneApplication.java
@@ -0,0 +1,13 @@
+package com.todd.lee.mccheyne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MccheyneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MccheyneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/todd/lee/mccheyne/MccheyneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todd.lee.mccheyne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MccheyneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
